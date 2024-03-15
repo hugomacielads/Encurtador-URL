ca6a3dc168d3b8935ce80b16bed4dba9a0673a49
hugomacielads <hugomacielcesar@gmail.com>
2024-03-15 16:46:45 -0300

    Resolução Ex5

--- Afrocódigos/Exercicios/Exercício5.py
+++ Afrocódigos/Exercicios/Exercício5.py
@@ -1,3 +1,19 @@
 # Exercícios
 # 
 # 5. Crie um programa que calcule o preço final de um produto após aplicar um desconto percentual fornecido pelo usuário.
+
+def Menu():
+    while True:
+        preco = float(input("Qual o preço do seu produto?"))
+        desconto = float(input("Qual a percentagem de desconto? (em %)"))
+
+        valor_desconto = (desconto / 100) * preco
+        preco_final = preco - valor_desconto
+
+        print(f"O preço do produto com desconto é: R${preco_final:.2f}")
+
+        continuar = input("Deseja calcular o desconto de outro produto? (s/n) ")
+        if continuar.lower() != 's':
+            break
+
+Menu()
