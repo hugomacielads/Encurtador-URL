26364217bfa7d61fd5ac1400c707fb9fb28b99c2
hugomacielads <hugomacielcesar@gmail.com>
2024-03-15 16:18:24 -0300

    Resolução Ex4

--- Afrocódigos/Exercicios/Exercício4.py
+++ Afrocódigos/Exercicios/Exercício4.py
@@ -1,3 +1,16 @@
 # Exercícios
 #
 # 4. Crie um programa que conte quantas vezes cada tipo de fruta aparece em uma lista.
+
+lista_frutas = ["Morango", "Abacaxi", "Uva", "Mamão", "Banana", "Maçã", "Pêra", "Uva", "Morango", "Morango", "Abacaxi"]
+
+def contador(lista):
+    dic_repetido = {}
+    for i in lista:
+        if i not in dic_repetido:
+            dic_repetido[i] = 1
+        else:
+            dic_repetido[i] += 1
+    print(dic_repetido)
+
+contador(lista_frutas)
